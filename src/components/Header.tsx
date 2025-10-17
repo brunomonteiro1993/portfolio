fa14af99e6671808a95152aa5ad359a146f6a63b
brunomonteiro1993 <brunogoncalvesmonteiro@gmail.com>
2025-10-17 14:54:29 -0300

    ⬆️ modo dark e ligth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,10 +14,8 @@ export function Header() {
     { name: 'Contato', href: '#contato' },
   ];
 
-  //teste
-
   return (
-    <header className="sticky top-0 z-50 bg-primary/80 backdrop-blur-md border-b border-theme">
+    <header className="sticky top-0 z-50 bg-primary/90 backdrop-blur-md border-b border-theme">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
